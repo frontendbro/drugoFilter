423630e38549b6609bb5d0d2990ac91c7f3d52c7
szadorozhniy <szadorozhniy@axiomsl.com>
2018-05-25 16:29:41 +0300

    add template

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ import 'styles/index.scss';
 import template from './template.hbs';
 
 let peoples = {
-  people: [
-    {firstName: "Yehuda", lastName: "Katz"},
-    {firstName: "Carl", lastName: "Lerche"},
-    {firstName: "Alan", lastName: "Johnson"}
-  ]
+    people: [
+        {firstName: "Yehuda", lastName: "Katz"},
+        {firstName: "Carl", lastName: "Lerche"},
+        {firstName: "Alan", lastName: "Johnson"}
+    ]
 };
 
 console.log(template(peoples));
