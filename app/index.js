bf5331747cd1e745c0ba65e2ff7590e8da8430ca
Sen4az <simon.z@yandex.ru>
2018-05-28 00:55:07 +0300

    add friends list

--- app/index.js
+++ app/index.js
@@ -1,16 +1,58 @@
 
 'use strict';
 import 'styles/index.scss';
-import template from './template.hbs';
+import template from './myfriends.hbs';
 
-let peoples = {
-    people: [
-        {firstName: "Yehuda", lastName: "Katz"},
-        {firstName: "Carl", lastName: "Lerche"},
-        {firstName: "Alan", lastName: "Johnson"}
-    ]
-};
+VK.init({
+    apiId: 6491959
+})
 
-console.log(template(peoples));
+function auth() {
+    return new Promise((resolve, reject) => {
+        VK.Auth.login(data => {
+            if(data.session) {
+                resolve();
+            } else {
+                reject(new Error('Ошибка авторизации'));
+            }
+        }, 2);
+    })
+}
 
-document.querySelector('#usersList').innerHTML = template(peoples);
+function callAPI(method, params) {
+    params.v = '5.76';
+
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if(data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response);
+            }
+        })
+    })
+}
+
+// (async () => {
+//     try {
+//         await auth();
+//         const [me] = await callAPI('users.get', { name_case: 'gen' });
+//         const friends = await callAPI('friends.get', {fields: 'city, country, photo_50'});
+//         document.querySelector('#myfriends-list').innerHTML = template(friends);
+//     } catch (e) {
+//         console.error(e);
+//     }
+// })();
+
+
+auth()
+    .then(() => {
+        return callAPI('users.get', { name_case: 'gen' });
+    })
+    .then(([me]) => {
+        return callAPI('friends.get', {fields: 'city, country, photo_50'})
+    })
+    .then(friends => {
+        console.log('​friends', friends);
+        document.querySelector('#myfriends-list').innerHTML = template(friends);
+    })
